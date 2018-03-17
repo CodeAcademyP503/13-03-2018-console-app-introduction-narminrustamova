e1e017bd149e4cc1caf87716b04837693d09816a
narminrustamova <37043699+narminrustamova@users.noreply.github.com>
2018-03-17 13:29:35 +0400

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1.Ekrandan girilen ededin nece reqemli oldugunu ekrana yazdiran proqram yazin
+
+            //int say;
+            //int i= 1;
+            //Console.Write("Heddini tapmaq istediyiniz reqemi daxil edin ");
+            //say = Convert.ToInt32(Console.ReadLine());
+            //while (say > 9)
+            //{
+            //    say = say / 10;
+            //    i++;
+
+            //}
+            //Console.Write("Say " + i + " Hedlidir");
+            //Console.ReadLine();
+
+
+            //2.Ekrandan girilen ededin reqemleri cemini ekrana yazdiran program yazin
+
+            //Console.Write("Reqem: ");
+            //string reqem = Console.ReadLine();
+            //int cem = 0;
+            //for (int i = 0; i < reqem.Length; i++)
+            //    cem += Convert.ToInt32(reqem[i].ToString());
+            //Console.WriteLine("Toplam:" + cem);
+
+
+            //3.Ekrandan daxil olunan ifadenin daxilinde sait ve samitleri ayri ayri ekrana yazdiran program yazin
+            //????
+
+            //4.Ekrandan daxil olunan cumlenin sozlerini tek tek ekrana cixaran program yazin
+            //????/
+
+            //string cumle;
+            //Console.Write("Cumle yazin ");
+            //cumle = Console.ReadLine();
+            //Console.Write(" {0} ", cumle);
+
+
+
+            //5.Menim keywords adinda bir listim var
+
+            //    string[] keyword = { "tenbel", "kod yazmayan", "yola veren", "esneyen" ,"bekar"};
+            //    string soz = Console.ReadLine();
+            //    if (axtar(keyword, soz))
+            //        Console.WriteLine(soz + " Siz losersinizr");
+            //    else
+            //        Console.WriteLine(soz + " siz loser deyilsiniz");
+            //}
+            //public static bool axtar(string[] array, string axtarilan)
+            //{
+            //    for (int i = 0; i < array.Length; ++i)
+            //        if (axtarilan.Equals(array[i])) return true;
+            //    return false;
+
+
+
+
+
+            //6. Ekrandan daxil edilen ifade icerisinde "tenbel" sozu varsa eyni ifadeni sadece tenbel sozunun evezine "loser" olaraq deyisdirerek yazdirmasini temin eden program yazin
+            //??
+          
+
+
+            //7.Bankomatdan pul cekmeye gedmisem. Ekrandan cekmek istediyim pul migdarini daxil edirem.Bankomat mene mumkun olan en boyuy eskinazlari vermelidir. meselen: 754 azn pul cekmek isteyiremse 7 eded yuzluk 1 eded 50 lik ve 4 eded birlik eskinaz vermelidir.Bunu reallasdiracaq proqram yazin.
+
+            //        int[] bankomat = { 100, 50, 20, 10, 5, 1 };
+            //    int pul, eded, i;
+            //    Console.Write("Miqdari daxil edin: ");
+            //    pul = Convert.ToInt32(Console.ReadLine());
+            //    Console.WriteLine("----------");
+            //    for (i = 0; i <= 5; i++)
+            //    {
+            //        eded = pul / bankomat[i];
+            //        if (eded != 0)
+            //            Console.WriteLine(eded.ToString() + " eded : " + Convert.ToString(bankomat[i] + " Azn "));
+            //        pul %= bankomat[i];
+            //    }
+            //    Console.ReadKey();
+        }
+    }
+}
